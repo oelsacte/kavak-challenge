2ec52a23cd77af281639a7b8d514f7908b2d1c89
OCTAVIO ELSACTE PALMA <oelsacte@gmail.com>
2020-06-08 15:33:53 -0500

    Algoritmo

--- src/emailsunicos.js
+++ src/emailsunicos.js
@@ -1,7 +1,21 @@
+
 function extraeId(email) {
     const token = email.split('@');
-    console.log(token);
-    return token;
+    return token[0];
 }
 
-const ex = extraeId('oelsacte@gmail.com')
+function extraePunto(token){
+    let i = token.indexOf('.');
+    lastPart = token.substr(i + 1);
+    firstPart = token.substr(0, i);
+    return firstPart + lastPart;
+}
+
+function transforms(email){
+    return extraePunto(extraeId(email));
+}
+
+var emailsUnicos = transforms('oelsa.cte@gmail.com');
+
+const ex = extraeId('oelsa.cte@gmail.com')
+console.log(emailsUnicos);
