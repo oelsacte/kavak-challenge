b539abf620ac302705d93beb10ea1512cb077628
OCTAVIO ELSACTE PALMA <oelsacte@gmail.com>
2020-06-09 19:32:07 -0500

    Se cierra el reto con el algoritmo solicitado

--- src/emailsunicos.js
+++ src/emailsunicos.js
@@ -1,21 +1,17 @@
-
-function extraeId(email) {
+var emailsUnicos = function(email) {
     const token = email.split('@');
-    return token[0];
+    const arrFromInput = token[0].split('');
+    var arrAux = '';
+    for(let i=0; i < arrFromInput.length; i++){
+        if(arrFromInput[i] == '+'){
+            break;
+        }
+        if(arrFromInput[i] !== `.`){
+            arrAux+= arrFromInput[i];
+        }
+    }
+    return arrAux.toString() + '@' +token[1];
 }
 
-function extraePunto(token){
-    let i = token.indexOf('.');
-    lastPart = token.substr(i + 1);
-    firstPart = token.substr(0, i);
-    return firstPart + lastPart;
-}
-
-function transforms(email){
-    return extraePunto(extraeId(email));
-}
-
-var emailsUnicos = transforms('oelsa.cte@gmail.com');
-
-const ex = extraeId('oelsa.cte@gmail.com')
-console.log(emailsUnicos);
+const ex = emailsUnicos('oe.lsa.cte@gmail.com')
+console.log(ex);
